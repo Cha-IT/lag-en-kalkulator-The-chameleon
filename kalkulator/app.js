17670e2176b2249e3e2c27908637f5600f129b0f
The-chameleon <alexanderthorenfeldt@outlook.com>
2024-09-03 14:17:55 +0200

    remove coments

--- kalkulator/app.js
+++ kalkulator/app.js
@@ -1,4 +1,4 @@
-// Først henter vi ut alle elementene vi skal bruke fra HTML
+
 const txtTall1 = document.querySelector("#tall1")
 const txtTall2 = document.querySelector("#tall2")
 const btnPluss = document.querySelector("#pluss")
@@ -31,13 +31,3 @@ btndele.onclick = function () {
     let sum = tall1 / tall2
     svarDiv.innerHTML = sum
 }
-/*
-    En alternativ måte å skrive en funksjon er å lage en "arrow function", slik:
-        () => { }
-    Dette btyr det samme som å skrive
-        function() { }
-        men det er bare en slags kortere måte å lage en funksjon, f.eks:
-        btnpluss.onclick = () => {
-        ...
-        }
-*/
