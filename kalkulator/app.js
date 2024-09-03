96a99c26984ad7441f207d7adb2d1429fdc65bef
The-chameleon <alexanderthorenfeldt@outlook.com>
2024-09-03 13:50:53 +0200

    fuk nye knapper

--- kalkulator/app.js
+++ kalkulator/app.js
@@ -2,6 +2,9 @@
 const txtTall1 = document.querySelector("#tall1")
 const txtTall2 = document.querySelector("#tall2")
 const btnPluss = document.querySelector("#pluss")
+const btnMinus = document.querySelector("#minus")
+const btngange = document.querySelector("#gange")
+const btndele = document.querySelector("#dele")
 const svarDiv = document.querySelector("#svar")
 
 btnPluss.onclick = function() {
@@ -10,7 +13,24 @@ btnPluss.onclick = function() {
     let sum = tall1 + tall2
     svarDiv.innerHTML = sum
 }
-
+btnMinus.onclick = function() {
+    let tall1 = Number(txtTall1.value);
+    let tall2 = Number(txtTall2.value);
+    let sum = tall1 - tall2
+    svarDiv.innerHTML = sum
+}
+btngange.onclick = function() {
+    let tall1 = Number(txtTall1.value);
+    let tall2 = Number(txtTall2.value);
+    let sum = tall1 * tall2
+    svarDiv.innerHTML = sum
+}
+btndele.onclick = function () {
+    let tall1 = Number(txtTall1.value);
+    let tall2 = Number(txtTall2.value);
+    let sum = tall1 / tall2
+    svarDiv.innerHTML = sum
+}
 /*
     En alternativ måte å skrive en funksjon er å lage en "arrow function", slik:
         () => { }
